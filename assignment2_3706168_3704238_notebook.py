7d28eaeb8bd5f7c5fd3b3eb6a84f63b6a8f10cc4
Niels345 <niels@looije.net>
2024-05-24 11:04:14 +0200

    1.1.2

--- assignment2_3706168_3704238_notebook.py
+++ assignment2_3706168_3704238_notebook.py
@@ -69,5 +69,126 @@ class FloodFillSolver():
                 steps.append(new_node)
         return steps
 
+############ CODE BLOCK 10 ################
+
+class GraphBluePrint:
+    def find_nodes(self): pass
+    def find_edges(self): pass
+
+class Graph(GraphBluePrint):
+    def __init__(self, map_, start=(0, 0)):
+        self.adjacency_list = {}
+        self.map = map_
+        self.start = start
+
+        self.find_nodes()
+        self.find_edges()  # This will be implemented in the next notebook cell
+
+    def find_nodes(self):
+        """
+        This method contains a breadth-frist search algorithm to find all the nodes in the graph.
+        """
+        queue = deque([self.start])
+        history = {self.start}
+
+        while queue:
+            current = queue.popleft()
+            actions = self.neighbour_coordinates(current)
+            self.adjacency_list_add_node(current, actions)
+
+            for next_node in actions:
+                if next_node not in history:
+                    history.add(next_node)
+                    queue.append(next_node)
+
+    def adjacency_list_add_node(self, coordinate, actions):
+        """
+        This is a helper function for the breadth-first search algorithm to add a coordinate to the `adjacency_list`.
+        """
+        if len(actions) != 2 or (len(actions) == 2 and actions[0][0] != actions[1][0] and actions[0][1] != actions[1][1]):
+            self.adjacency_list[coordinate] = set()
+
+    def neighbour_coordinates(self, coordinate):
+        """
+        This method returns the next possible actions and is part of the breadth-first search algorithm.
+        """
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        steps = []
+
+        for dx, dy in directions:
+            new_node = (coordinate[0] + dx, coordinate[1] + dy)
+            if 0 <= new_node[0] < self.map.grid.shape[0] and 0 <= new_node[1] < self.map.grid.shape[1]:
+                if self.map.grid[new_node[0], new_node[1]] != 0:
+                    steps.append(new_node)
+
+        return steps
+
+    def __repr__(self):
+        return repr(dict(sorted(self.adjacency_list.items()))).replace("},", "},\n")
+
+    def __getitem__(self, key):
+        return self.adjacency_list[key]
+
+    def __contains__(self, key):
+        return key in self.adjacency_list
+
+    def get_random_node(self):
+        return tuple(np.random.choice(list(self.adjacency_list)))
+
+    def show_coordinates(self, size=5, color='k'):
+        nodes = self.adjacency_list.keys()
+        plt.plot([n[1] for n in nodes], [n[0] for n in nodes], 'o', color=color, markersize=size)
+
+    def show_edges(self, width=0.05, color='r'):
+        for node, edge_list in self.adjacency_list.items():
+            for next_node, _, _ in edge_list:
+                plt.arrow(node[1], node[0], (next_node[1] - node[1]) * 0.975, (next_node[0] - node[0]) * 0.975, color=color, length_includes_head=True, width=width, head_width=4 * width)
+
+############ CODE BLOCK 15 ################
+    def __init__(self, map_, start=(0, 0)):
+        self.adjacency_list = {}
+        self.map = map_
+        self.start = start
+        self.road_grid = map_.grid
+
+        self.find_nodes()
+        self.find_edges()  # This will be implemented in the next notebook cell
+          
+    def find_edges(self):
+        """
+        This method does a depth-first/brute-force search for each node to find the edges of each node.
+        """
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        for node in self.adjacency_list:
+            for direction in directions:
+                next_node, distance = self.find_next_node_in_adjacency_list(node, direction)
+                if next_node in self.adjacency_list and distance > 0:
+                    speed_limit = self.road_grid[node[0], node[1]]
+                    self.adjacency_list[node].add((next_node, distance, speed_limit))
+                    self.adjacency_list[next_node].add((node, distance, speed_limit))
+
+    def find_next_node_in_adjacency_list(self, node, direction):
+        """
+        This is a helper method for find_edges to find a single edge given a node and a direction.
+
+        :param node: The node from which we try to find its "neighboring node" NOT its neighboring coordinates.
+        :type node: tuple[int]
+        :param direction: The direction we want to search in this can only be 4 values (0, 1), (1, 0), (0, -1) or (-1, 0).
+        :type direction: tuple[int]
+        :return: This returns the first node in this direction and the distance.
+        :rtype: tuple[int], int 
+        """
+        current = node
+        distance = 0
+        rows, cols = self.road_grid.shape
+        while True:
+            next_node = (current[0] + direction[0], current[1] + direction[1])
+            if (next_node[0] < 0 or next_node[0] >= rows or
+                next_node[1] < 0 or next_node[1] >= cols or
+                next_node in self.adjacency_list):
+                return next_node, distance
+            current = next_node
+            distance += 1
+
 
 ############ END OF CODE BLOCKS, START SCRIPT BELOW! ################
